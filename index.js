826332055ba51324c14f02ae10fb80eecd876a88
Thomas Delaet <thomas@delaet.org>
2016-08-06 22:39:42 +0200

    initial code commit

--- index.js
+++ index.js
@@ -1,7 +1,112 @@
 'use strict'
 
-var TenantService = function (options) {}
+var assert = require('assert')
+var _ = require('lodash')
+var identityHelpers = require('mm-create-identity')
 
-console.log(TenantService)
+var TenantService = function (options) {
+  assert(_.isObject(options))
+  assert(_.isObject(options.runtime))
+  assert(_.isObject(options.platform))
+  assert(_.isFunction(options.runtimeClass))
+  assert(_.isObject(options.storage))
+  assert(_.isString(options.secret))
+  assert(_.isObject(options.logger))
+  this._log = options.logger
+  this._secret = options.secret
+  this._runtime = options.runtime
+  this._runtimeClass = options.runtimeClass
+  this._storage = options.storage
+  this._platform = options.platform
+  this._tenants = {}
+  this.platform.messaging.on('public.tenant.create', this._onCreate.bind(this))
+  this._loadTenants()
+  this._ready = false
+}
+
+TenantService.prototype._loadTenants = function () {
+  var self = this
+  this.storage.get('tenants', function (err, result) {
+    if (!err) {
+      var tenants = JSON.parse(result)
+      _.forEach(tenants, function (tenant) {
+        var environment = {}
+        if (self._runtime._hasPersistence()) {
+          environment.PERSISTENCE = self._runtime.appendToPersistence(tenant)
+        }
+        var runtime = new self._runtimeClass(environment)
+        runtime.createPlatform()
+        runtime.platform.on('ready', function () {
+          runtime.createService('serviceManager')
+        })
+        self._tenants[tenant] = runtime
+      })
+    }
+    self._ready = true
+  })
+}
+
+TenantService.prototype._saveTenants = function () {
+  this.storage.put('tenants', JSON.stringify(_.keys(this._tenants)))
+}
+
+TenantService.prototype._validMessage = function (data) {
+  return this._ready &&
+    _.isObject(data) &&
+    _.isString(data.secret) &&
+    data.secret === this._secret &&
+    _.isString(data.id)
+}
+
+TenantService.prototype._onCreate = function (topic, publicKey, data) {
+  if (this._validMessage(data)) {
+    if (this._tenantExists(publicKey)) {
+      var tenantKey = this._getTenantKey(publicKey)
+      this.platform.messaging.send(publicKey, tenantKey, data.id)
+    } else {
+      this._createNewTenant(publicKey, data.id)
+    }
+  }
+}
+
+TenantService.prototype._getTenantKey = function (publicKey) {
+  _.forEach(this._tenants, function (runtime, tenantKey) {
+    if (runtime.services.devices.inScope(publicKey)) {
+      return tenantKey
+    }
+  })
+}
+
+TenantService.prototype._tenantExists = function (publicKey) {
+  return !_.isUndefined(this._getTenantKey(publicKey))
+}
+
+TenantService.prototype._sendReply = function (destination, tenantKey, id) {
+  this.platform.messaging.send('tenant.createReply', destination, {
+    id: id,
+    publicKey: tenantKey
+  })
+}
+
+TenantService.prototype._createNewTenant = function (publicKey, id) {
+  var self = this
+  var keyPair = identityHelpers.createKeyPair()
+  var environment = {
+    SERVICES: 'kademlia mdns serviceManager flukso devices',
+    IDENTITY: keyPair.secretKey
+  }
+  if (this._runtime._hasPersistence()) {
+    environment.PERSISTENCE = this._runtime.appendToPersistence(keyPair.publicKey)
+  }
+  var runtime = new this._runtimeClass(environment)
+  runtime.createPlatform()
+  runtime.platform.on('ready', function () {
+    var tenantKey = runtime.platform.identity.getSignId()
+    self._tenants[tenantKey] = runtime
+    self._saveTenants()
+    runtime.services.devices.addKey(publicKey)
+    self._sendReply(publicKey, tenantKey, id)
+  })
+}
 
 module.exports = TenantService
--- test/index.unit.js
+++ test/index.unit.js
@@ -3,7 +3,9 @@
 var chai = require('chai')
 var expect = chai.expect
 var _ = require('lodash')
+var winston = require('winston')
 
+var PlatformStub = require('mm-platform-stub')
 var TenantService = require('../index.js')
 
 describe('mm-services-tenant', function () {
@@ -11,5 +13,34 @@ describe('mm-services-tenant', function () {
     it('should define TenantService class', function () {
       expect(_.isFunction(TenantService)).to.be.true
     })
+    it('should create TenantService class with platform and logger parameter', function () {
+      var platform = new PlatformStub()
+      var tenantService = new TenantService({
+        platform: platform,
+        logger: winston
+      })
+      expect(tenantService).to.be.an('object')
+      expect(tenantService).to.have.property('platform')
+      expect(tenantService.platform).to.equal(platform)
+      expect(tenantService).to.have.property('_log')
+      expect(tenantService._log).to.equal(winston)
+    })
   })
+/*
+            describe('create', function() {
+                describe('receive create', function() {
+                    var nodeA = new TenantService({
+                        platform: new PlatformStub({
+                            identity: 'nodeA'
+                        }),
+                        logger: winston
+                    })
+                    var nodeB = new TenantService({
+                        platform: new PlatformStub({
+                            identity: 'nodeB'
+                        }),
+                        logger: winston
+                    })
+                })
+                */
 })

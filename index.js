f2467f2d7df80b40587035b68f1ae4e1dcc7426c
Thomas Delaet <thomas@delaet.org>
2016-08-12 22:10:48 +0200

    fixes persistence logic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var assert = require('assert')
 var _ = require('lodash')
 var identityHelpers = require('mm-create-identity')
+var uuid = require('node-uuid')
 
 var TenantService = function (options) {
   assert(_.isObject(options))
@@ -29,17 +30,17 @@ TenantService.prototype._loadTenants = function () {
   this._storage.get('tenants', function (err, result) {
     if (!err) {
       var tenants = JSON.parse(result)
-      _.forEach(tenants, function (tenant) {
+      _.forEach(tenants, function (tenantName) {
         var environment = {}
         if (self._runtime._hasPersistence()) {
-          environment.PERSISTENCE = self._runtime.appendToPersistence(tenant)
+          environment.PERSISTENCE = self._runtime.appendToPersistence(tenantName)
         }
         var runtime = new self._runtimeClass(environment)
         runtime.createPlatform()
         runtime.platform.on('ready', function () {
           runtime.createService('serviceManager')
         })
-        self._tenants[tenant] = runtime
+        self._tenants[tenantName] = runtime
       })
     }
     self._ready = true
@@ -62,7 +63,7 @@ TenantService.prototype._onCreate = function (topic, publicKey, data) {
   if (this._validMessage(data)) {
     if (this._tenantExists(publicKey)) {
       var tenantKey = this._getTenantKey(publicKey)
-      this.platform.messaging.send(publicKey, tenantKey, data.id)
+      this._sendReply(publicKey, tenantKey, data.id)
     } else {
       this._createNewTenant(publicKey, data.id)
     }
@@ -72,7 +73,7 @@ TenantService.prototype._onCreate = function (topic, publicKey, data) {
 TenantService.prototype._getTenantKey = function (publicKey) {
   _.forEach(this._tenants, function (runtime, tenantKey) {
     if (runtime.services.devices.inScope(publicKey)) {
-      return tenantKey
+      return runtime.platform.identity.getBoxId()
     }
   })
 }
@@ -92,20 +93,22 @@ TenantService.prototype._createNewTenant = function (publicKey, id) {
   var self = this
   var keyPair = identityHelpers.createKeyPair()
   var environment = {
-    SERVICES: 'kademlia mdns serviceManager flukso devices',
+    SERVICES: 'kademlia mdns serviceManager flukso devices events',
     IDENTITY: keyPair.secretKey
   }
+  var tenantName = uuid.v4()
   if (this._runtime._hasPersistence()) {
-    environment.PERSISTENCE = this._runtime.appendToPersistence(keyPair.publicKey)
+    environment.PERSISTENCE = this._runtime.appendToPersistence(tenantName)
   }
   var runtime = new this._runtimeClass(environment)
   runtime.createPlatform()
   runtime.platform.on('ready', function () {
-    var tenantKey = runtime.platform.identity.getSignId()
-    self._tenants[tenantKey] = runtime
+    var tenantKey = runtime.platform.identity.getBoxId()
+    self._tenants[tenantName] = runtime
     self._saveTenants()
     runtime.services.devices.addKey(publicKey)
     self._sendReply(publicKey, tenantKey, id)
+    runtime.activateServices()
   })
 }
 

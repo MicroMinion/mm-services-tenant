6951060311bb0e5c3f0f680cb6f8249b4881ec87
Marc Bachmann <marc.brookman@gmail.com>
2016-11-23 02:08:51 +0100

    chore(package): update uuid to version 3.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var assert = require('assert')
 var _ = require('lodash')
 var identityHelpers = require('mm-create-identity')
-var uuid = require('node-uuid')
+var uuid = require('uuid')
 var winstonWrapper = require('winston-meta-wrapper')
 
 var TenantService = function (options) {
